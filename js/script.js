dc8f3672619ffe28241f51f3ee6f231b09b0f918
stasyasep <natashasep090990@gmail.com>
2018-10-14 14:50:43 +0300

    comments added

--- js/script.js
+++ js/script.js
@@ -51,33 +51,61 @@ let j = /\.+\w+\s/g
 //   <p><input type="submit" value="Відправити"></p>
 // </form>
 
-
-// let n = document.querySelector('#fmail');
-// let g = document.querySelector('#ftel');
-
-// g.addEventListener("input", function() {
-
-// 	let k = document.querySelector('#fmail').value;
-
-// 	if(re.test(k)){
-// 		console.log("true");
-// 	}
-
-// 	let f = document.querySelector('#ftel').value;
-
-// 	if(tel.test(f)){
-// 		console.log("true2");
-// 	}
-
-// 	console.log(f)s;
-
-
-//   });
+// Напишіть функцію валідації імені, ім'я повинно складатися лише з латинських літер, без пробілів, перша літера велика.
+// function validName(s){
+//   var err = 'Невірний формат імені!\nФормат: латинські літери, перша - велика.';   ...
+//   якщо введено невірні дані: alert(err);
+//   return true або false;
+// }
+// Доступ до введених користувачем даних здійснюється таким чином:
+// змінна = document.getElementById('ідентифікатор').value;
+// var a = document.getElementById('fname').value;
+// Напишіть функцію валідації e-mail.
+// Логін повинен складатися з латинських символів, символів мінус, підкреслювання та крапка.
+// Домен повинен складатися лише з латинських символів.
+// Доменна зона може складатися з 2-5 латинських символів.
+// Регістр не має значення.
+// Напишіть функцію фалідації телефона.
+// Пробіли, дужки, мінуси, можуть бути відсутні.
+// +380 - обов'язкові цифри, приймаються телефони лише України, 12 цифр.
+// +380682091234
+// +38068 209-12-34
+// +3 8 (068) 209 12 34
+// +38(068)209-12-34
+// і т.п.
+// Створіть валідатор форми:
+// document.getElementById("form1").onsubmit = function() {
+//   // напишіть алгоритм валідатора
+//   // створіть код по алгоритму
+// }
+// Якщо в функції onsubmit використати оператор return false;, то відправка даних на сервер перерветься, дані не будуть відправлені.
 
 
 let nameFormat = /^[A-Z][a-z]+$/;
 let mailFormat = /^\w+@\w+\.\w{2,5}$/i;
 let telFormat = /^\+3 ?8 ?\(?0\d\d ?\)? ?\d{3}[- ]?\d\d[- ]?\d\d$/;
+
+
+document.getElementById("form1").onsubmit = function(even){
+
+	event.preventDefault();
+
+	let name = document.getElementById('fname').value;
+	let mail = document.getElementById('fmail').value;
+	let tel = document.getElementById('ftel').value;
+
+	if(validName(name)&&validMail(mail)&&validTelephone(tel)){
+
+		alert("Форму відправлено!");
+		return true;
+	}
+
+	else{
+		
+		alert("Форму не відправлено!");
+		return false;
+	}
+}
 
 
 function validName(s){
@@ -131,26 +159,7 @@ function validTelephone(t){
 	}
 }
 
-document.getElementById("form1").onsubmit = function(even){
 
-	event.preventDefault();
-
-	let name = document.getElementById('fname').value;
-	let mail = document.getElementById('fmail').value;
-	let tel = document.getElementById('ftel').value;
-
-	if(validName(name)&&validMail(mail)&&validTelephone(tel)){
-
-		alert("Форму відправлено!");
-		return true;
-	}
-
-	else{
-		
-		alert("Форму не відправлено!");
-		return false;
-	}
-}
 
 
 
